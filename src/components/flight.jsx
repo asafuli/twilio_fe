304089e61c4b19f4e4d61eae521af8577c6b46c2
Assaf Eli <ASSAFEL@amdocs.com>
2018-12-17 14:21:41 +0200

    switching to react-google-maps

--- src/components/flight.jsx
+++ src/components/flight.jsx
@@ -27,7 +27,7 @@ const Flight = props => {
         process.env.REACT_APP_MAPS_API_KEY
       }&v=3.exp&libraries=geometry,drawing,places`}
       loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `600px` }} />}
       mapElement={<div style={{ height: `100%` }} />}
     />
   );
