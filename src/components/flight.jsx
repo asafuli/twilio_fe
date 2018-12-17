6adf0fa51b68051df826baf90c6abe8d548db72f
Assaf Eli <ASSAFEL@amdocs.com>
2018-12-17 13:44:14 +0200

    switching to react-google-maps

--- src/components/flight.jsx
+++ src/components/flight.jsx
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+
+const Flight = props => {
+  return (
+    <MyMapComponent
+      isMarkerShown
+      googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  );
 };
 
-export class Flight extends Component {
-  render() {
-    console.log(process.env);
-    return (
-      <Map
-        google={this.props.google}
-        style={mapStyles}
-        zoom={9}
-        initialCenter={{ lat: -1.23, lng: 36.2 }}
-      />
-    );
-  }
-}
-
-export default GoogleApiWrapper({
-  apikey: `${process.env.REACT_APP_MAPS_API_KEY}`
-})(Flight);
+export default Flight;
