990cda998300d89ff87f84114ec1288bdde1c5a2
Assaf Eli <ASSAFEL@amdocs.com>
2018-12-17 13:55:08 +0200

    switching to react-google-mapss

--- src/components/flight.jsx
+++ src/components/flight.jsx
@@ -20,7 +20,9 @@ const Flight = props => {
   return (
     <MyMapComponent
       isMarkerShown
-      googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${
+        process.env.REACT_APP_MAPS_API_KEY
+      }&v=3.exp&libraries=geometry,drawing,places`}
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
       mapElement={<div style={{ height: `100%` }} />}
