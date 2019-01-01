00aa6728a9f27dceb2aa1436d979e26fc60b4bbe
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-01 22:14:46 +0200

    adding styled compinenets

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -18,6 +18,7 @@ const StyledPinsItem = styled.li`
 
 const SideBar = ({ className }) => {
   return (
+    <h1 className={className}></h1>
     <StyledPinsList className={className}>
       <StyledPinsItem>Accomodation</StyledPinsItem>
       <StyledPinsItem>Europe park</StyledPinsItem>
