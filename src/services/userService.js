53559afa01b45a4bf8925d857cb28b4125ca88e7
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-02 14:46:07 +0200

    extracting userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,11 @@ import http from './httpService';
 import config from '../config/config';
 
 export async function getUserInfo(uid) {
-  const res = await http.get(`${config.serverUrl}\\user\\${uid}`);
+  const res = await http.get(`${config.serverUrl}\\user\\${uid}`, {
+    params: {
+      preventCache: new Date()
+    }
+  });
   const { advice, user, resource, message } = res.data;
   //console.log(message);
   return { advice, user, resource, message };
