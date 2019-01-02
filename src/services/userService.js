e4d137df01277a8c4e4fc785fee802b770a770cc
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-02 14:10:51 +0200

    extracting userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,6 @@ import config from '../config/config';
 export async function getUserInfo(uid) {
   const res = await http.get(`${config.serverUrl}\\user\\${uid}`);
   const { advice, user, resource, message } = res.data;
-  console.log(message);
+  //console.log(message);
   return { advice, user, resource, message };
 }
