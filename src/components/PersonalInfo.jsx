82e097cdf5732f1b5f6b3674b83eeb8a5bb23694
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-03 14:35:45 +0200

    fixing serverURL to https

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,64 +1,7 @@
 import React from 'react';
 
 const PersonalInfo = ({ uid }) => {
-  state = {
-    advice: '',
-    user: '',
-    resource: '',
-    message: [],
-    showMessages: false,
-    secondsToFlight: 0
-  };
-
-  async componentDidMount() {
-    const uid = this.props.match.params.id;
-    const userInfo = await getUserInfo(uid);
-    this.setState(userInfo);
-    const targetDate = new Date(2019, 4, 13, 12, 0, 0);
-    setInterval(this.countdown, 1000, targetDate);
-  }
-
-  countdown = targetDate => {
-    const currentDate = new Date();
-    const secondsToFlight = Math.ceil(
-      (targetDate.valueOf() - currentDate.valueOf()) / 1000
-    );
-    this.setState({ secondsToFlight });
-  };
-
-  toggleShowMsg = async () => {
-    const uid = this.props.match.params.id;
-    const userInfo = await getUserInfo(uid);
-    this.setState({ ...userInfo, showMessages: !this.state.showMessages });
-  };
-
-  render() {
-    const { advice, user, resource, message } = this.state;
-    
-      <div className='container'>  
-         <ul>
-          {message.map((msg, idx) => {
-            if (msg.toLowerCase().includes('www')) {
-              return (
-                <div key={idx}>
-                  <li>
-                    <strong>
-                      <a href={msg}>{msg}</a>
-                    </strong>
-                  </li>{' '}
-                </div>
-              );
-            } else
-              return (
-                <div key={idx}>
-                  <li>
-                    <strong>{msg}</strong>
-                  </li>{' '}
-                </div>
-              );
-          })}
-        </ul>
-      </div>     
+  return <p>{uid}</p>;
 };
 
 export default PersonalInfo;
