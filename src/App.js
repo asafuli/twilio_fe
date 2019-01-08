6e3be6f41973f2542164efd705400a6ee1609fa8
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-08 17:24:49 +0200

    upgrade to 16.7

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Flight from './components/flight';
 import NavBar from './components/navbar';
 import Map from './components/map';
 import PersonalInfo from './components/PersonalInfo';
-import styledAccomodation  from './components/accomodation';
+import styledAccomodation from './components/accomodation';
 import { getCurrentUser } from './services/userService';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -13,7 +13,6 @@ class App extends Component {
 
   componentDidMount = () => {
     const pathArray = window.location.pathname.split('/');
-    console.log(pathArray);
     this.setState({ uid: pathArray[2] });
   };
 
