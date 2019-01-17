b00a5415695509aa10eaaff7bb03a141398a9504
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-17 13:19:35 +0200

    adding redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  advice: '',
+  user: '',
+  resource: '',
+  message: [],
+  showMessages: false,
+  secondsToFlight: 0
+};
+
+function rootReducer(state = initialState, action) {
+  return state;
+}
+
+export default rootReducer;
