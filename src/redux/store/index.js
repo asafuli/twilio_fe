a255afee7ca271cea1ad44d5724ecb15a502ced3
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-21 17:27:41 +0200

    REDUX DEVTOOLS

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { thunkMiddleware } from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { hideNavOnScrollMW } from '../middleware/hideNavOnScrollMW';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer, applyMiddleware(hideNavOnScrollMW));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
