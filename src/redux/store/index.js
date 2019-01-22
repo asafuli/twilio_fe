505254be7816f0a5275236dd084d2e4d3f688a39
Assaf Eli <ASSAFEL@amdocs.com>
2019-01-22 22:10:53 +0200

    compose with devtool

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,11 @@
 import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from '../reducers/index';
 
 const store = createStore(
   rootReducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunkMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
