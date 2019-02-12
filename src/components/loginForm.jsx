96cbe0d65e6e348cfa87015d0e402b32bbcf84c1
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-12 15:47:45 +0200

    connect LoginForm

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -67,7 +67,12 @@ class connectedLoginForm extends Form {
   }
 }
 
-const LoginForm = withRouter(connect(connectedLoginForm));
+const LoginForm = withRouter(
+  connect(
+    mapStateToProps,
+    connectedLoginForm
+  )
+);
 
 export default LoginForm;
 //export default withCookies(LoginForm);
