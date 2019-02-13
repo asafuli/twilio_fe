6d81c025770f1ed3e06fde3add7b00269b46028b
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-13 14:14:23 +0200

    login navbar right

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -60,7 +60,7 @@ const ConnectedNavBar = props => {
               </NavLink>
             </li>
           </ul>
-          <ul className='navbar-nav navbar-right mr-auto'>
+          <ul className='navbar-nav navbar-right'>
             <li className='nav-item'>
               <NavLink className='nav-link' to='/login'>
                 Login
