6dff5024d1d9d4758ebe19d9300724004a4770f4
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-26 15:42:04 +0200

    socket.io-client

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import io from 'socket.io-client';
+import config from '../config/config';
 
 const PersonalInfo = ({ uid }) => {
-  return <p>{uid}</p>;
+  const chatConnect = () => {
+    const socket = io(config.serverUrl);
+  }
+  return (
+    <div>
+      <p>{uid}</p>
+      <button className="secondary" onClick={() => chatConnect()} >
+        Connect to chat
+      </button>
+    </div>
+  )
+  
 };
 
 export default PersonalInfo;
+
+
+
