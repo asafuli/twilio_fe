2dd3013b815338292be621a8c4eb474478b3dbd4
Assaf Eli <ASSAFEL@amdocs.com>
2019-02-26 16:05:20 +0200

    socket.io-client

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 import config from '../config/config';
 
 const PersonalInfo = ({ uid }) => {
+  const { chatMessages, setChatMessages } = useState([]);
+
   const chatConnect = () => {
     const socket = io(config.serverUrl);
-  }
+    socket.on('chat message', msg => setChatMessages(chatMessages.push(msg)));
+  };
   return (
     <div>
       <p>{uid}</p>
-      <button className="secondary" onClick={() => chatConnect()} >
+      <button className='secondary' onClick={() => chatConnect()}>
         Connect to chat
       </button>
+      <h3>Chat messages!</h3>
+      <ul>
+        {chatMessages.map(msg => (
+          <li>msg</li>
+        ))}
+      </ul>
     </div>
-  )
-  
+  );
 };
 
 export default PersonalInfo;
-
-
-
