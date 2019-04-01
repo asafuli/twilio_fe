1be3bd31585d2a7466b61164e869d8b545085663
Assaf Eli <ASSAFEL@amdocs.com>
2019-04-01 14:53:50 +0300

    fixing responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -29,29 +29,27 @@ const ConnectedNavBar = props => {
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='mr-auto '>
-          <Nav.Link>
+          <Nav.Link componentClass='span'>
             <NavLink className='nav-link' to='/flight'>
               Flight
             </NavLink>
           </Nav.Link>
-          <Nav.Link>
+          <Nav.Link componentClass='span'>
             <NavLink className='nav-link' to='/accomodation'>
               Accomodation
             </NavLink>
           </Nav.Link>
-          <Nav.Link>
-            <div>
-              <NavLink className='nav-link' to='/map'>
-                Map
-              </NavLink>
-            </div>
+          <Nav.Link componentClass='span'>
+            <NavLink className='nav-link' to='/map'>
+              Map
+            </NavLink>
           </Nav.Link>
-          <Nav.Link>
+          <Nav.Link componentClass='span'>
             <NavLink className='nav-link' to='/home'>
               My Page
             </NavLink>
           </Nav.Link>
-          <Nav.Link>
+          <Nav.Link componentClass='span'>
             <NavLink className='nav-link' to='/chat'>
               Chat
             </NavLink>
@@ -59,12 +57,12 @@ const ConnectedNavBar = props => {
         </Nav>
         {!props.loggedIn && (
           <Nav>
-            <Nav.Link>
+            <Nav.Link componentClass='span'>
               <NavLink className='nav-link' to='/login'>
                 Login
               </NavLink>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link componentClass='span'>
               <NavLink className='nav-link' to='/home'>
                 Register
               </NavLink>
@@ -73,12 +71,12 @@ const ConnectedNavBar = props => {
         )}
         {props.loggedIn && (
           <Nav>
-            <Nav.Link>
+            <Nav.Link componentClass='span'>
               <navLink className='nav-link active' to='/home'>
                 {props.user}
               </navLink>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link componentClass='span'>
               <NavLink className='nav-link' to='/Logout'>
                 Logout
               </NavLink>
