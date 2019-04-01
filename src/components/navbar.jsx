27eb816487780aced622a2c1e63df0e5bffb134a
Assaf Eli <ASSAFEL@amdocs.com>
2019-04-01 13:48:51 +0300

    responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,72 +19,32 @@ const MapStateToProps = ({
 const ConnectedNavBar = props => {
   return (
     <div>
-      <Navbar
-        collapseOnSelect
-        expand='lg'
-        bg='dark'
-        variant='dark'
-        // className={props.navBarClass}
-      >
-        <Navbar.Brand href='#home'>The Black Forest X</Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='mr-auto '>
-            <NavLink className='nav-link' to='/flight'>
-              Flight
-            </NavLink>
-
-            {/*<Nav.Link href='#features'>
-            <NavLink className='nav-link' to='/accomodation'>
-              Accomodation
-            </NavLink>
-          </Nav.Link>
-          <Nav.Link href='#features'>
-            <NavLink className='nav-link' to='/map'>
-              Map
-            </NavLink>
-          </Nav.Link>
-          <Nav.Link href='#features'>
-            <NavLink className='nav-link' to='/home'>
-              My Page
-            </NavLink>
-          </Nav.Link>
-          <Nav.Link href='#features'>
-            <NavLink className='nav-link' to='/chat'>
-              Chat
-            </NavLink>
-          </Nav.Link> */}
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-          {!props.loggedIn && (
-            <Nav>
-              {/* <Nav.Link href='#deets'>
-              <NavLink className='nav-link' to='/login'>
-                Login
-              </NavLink>
-            </Nav.Link>
-            <Nav.Link href='#deets'>
-              <NavLink className='nav-link' to='/home'>
-                Register
-              </NavLink>
-            </Nav.Link> */}
-            </Nav>
-          )}
-          {props.loggedIn && (
-            <Nav>
-              {/* <Nav.Link href='#deets'>
-              <navLink className='nav-link active' to='/home'>
-                {props.user}
-              </navLink>
-            </Nav.Link>
-            <Nav.Link href='#deets'>
-              <NavLink className='nav-link' to='/Logout'>
-                Logout
-              </NavLink>
-            </Nav.Link> */}
-            </Nav>
-          )}
+          <Form inline>
+            <FormControl type='text' placeholder='Search' className='mr-sm-2' />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Navbar>
+      ;
     </div>
   );
 };
