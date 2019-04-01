4cd0694df66f819005d094f1046f2372c5e212ea
Assaf Eli <ASSAFEL@amdocs.com>
2019-04-01 15:17:34 +0300

    fixing responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -29,14 +29,12 @@ const ConnectedNavBar = props => {
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='mr-auto '>
-          <Nav.Link
-            is={NavLink}
-            href='#features'
-            className='nav-link'
-            to='/flight'
-          >
+          <Nav.Link as={NavLink} className='nav-link' to='/flight'>
             Flight
           </Nav.Link>
+          <Nav.Item as={NavLink} className='nav-item' to='/map'>
+            map
+          </Nav.Item>
           {/* <Nav.Link href='#features'>
             <NavLink className='nav-link' to='/accomodation'>
               Accomodation
